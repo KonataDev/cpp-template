a7451b3e0fe9a6c75a502c15814c108e70fa49a0
残象之形 <976999484@qq.com>
2023-05-29 14:08:23 +0800

    build(cmake): update config

--- tests/include/test.h
+++ tests/include/test.h
@@ -3,12 +3,3 @@
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <fmt/core.h>
-
-template<typename T>
-constexpr ::std::string_view type() noexcept
-{
-    return __func__; // NOLINT
-}
-
-constexpr auto only_true = [](const auto&) { return true; };
-constexpr auto only_false = [](const auto&) { return false; };
